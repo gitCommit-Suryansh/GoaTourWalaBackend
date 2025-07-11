90020f8309781cabb5bf2880dec8408a6dac0e93
BlackHat86 <suryansh.c05@gmail.com>
2025-07-11 20:27:38 +0530

    added cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const categoryRoutes = require('./routes/categoryRoutes.js');
 
 
 
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: ['*', 'https://goa-tour-wala-frontend.vercel.app/'], credentials: true }));
 app.use(express.json());
 
 connectToDB();
